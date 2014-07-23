3e02a4861aa39b74910e3c58b0cf265e34a421fa
Justin Hedani <jkhedani@gmail.com>
2014-07-22 17:04:02 -1000

    Correct placement of analytics text.

--- functions.php
+++ functions.php
@@ -598,19 +598,4 @@ function register_shortcodes() {
 }
 add_action('init','register_shortcodes');
 
-/**
- *  Add Google Analytics to the bottom of the page
- */
-function google_track_analytics() { ?>
-    <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-53110929-1', 'auto');
-  ga('send', 'pageview');
-    </script>
-<?php
-}
-add_action( 'wp_footer', 'google_track_analytics' );
 ?>
