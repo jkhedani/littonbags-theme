3386065be8a5d806afa4a4e67bb8f2c170505a0b
Justin Hedani <jkhedani@gmail.com>
2014-09-28 08:06:19 -1000

    Crispify logo resolution

--- header.php
+++ header.php
@@ -62,7 +62,7 @@ $page_keywords = 'women\'s camera bags, women\'s camera purse, leather camera ba
         <div class="container">
 
           <!-- Site Logo -->
-          <a class="site-title" href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+          <a class="site-title" href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/litton-ludwig-logo@2X.png" alt="Litton Fine Camera Bags" /></a>
 
           <!-- Hand Basket Toggle -->
           <a href="javascript:void(0);" data-toggle="hand-basket"><i class="fa fa-shopping-cart"></i></a>
