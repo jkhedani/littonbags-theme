680ad4f110a16f3e40c5bac68327084823ed2478
Justin Hedani <jkhedani@gmail.com>
2014-09-28 07:45:27 -1000

    Create and add favicon

--- header.php
+++ header.php
@@ -33,7 +33,7 @@ $page_keywords = 'women\'s camera bags, women\'s camera purse, leather camera ba
 	<meta name="keywords" content="<?php echo $page_keywords; ?>" />
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	<link rel="shortcut icon" href="<?php bloginfo( 'stylesheet_directory' ); ?>/images/favicon.png" />
+	<link rel="shortcut icon" href="<?php bloginfo( 'stylesheet_directory' ); ?>/images/favicon-32x32.png" />
 	<script type="text/javascript" src="https://js.stripe.com/v2/"></script>
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/inc/js/html5.js" type="text/javascript"></script>
